66e4ed0aba0afb969e0534448c65e50aa1da46e7
Mykhailo Tokmakov <mishkabalooo@gmail.com>
2024-09-28 17:56:11 +0300

    DownloadImageAsync Bootcamp

---
+++ SwiftConcurrencyBootcamp/DownloadImageAsync.swift
@@ -0,0 +1,18 @@
+//
+//  DownloadImageAsync.swift
+//  SwiftConcurrencyBootcamp
+//
+//  Created by Michael on 9/28/24.
+//
+
+import SwiftUI
+
+struct DownloadImageAsync: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DownloadImageAsync()
+}
